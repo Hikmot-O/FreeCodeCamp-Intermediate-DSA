71057fccee121834c499d1492f978d982f3d4238
Hikmot <69507776+Hikmot-O@users.noreply.github.com>
2022-11-08 10:55:01 +0100

    first commit

---
+++ script .js
@@ -0,0 +1,36 @@
+"use strict";
+
+//Intermediate DSA 1//
+function sumAll(arr) {
+  let sum = 0;
+
+  const sortNumbers = (a, b) => {
+    return a - b;
+  };
+  arr.sort(sortNumbers);
+
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    sum += i;
+  }
+  return sum;
+}
+sumAll([4, 1]);
+
+//Intermediate DSA 2//
+function diffArray(arr1, arr2) {
+  let newArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      newArr.push(arr1[i]);
+    }
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (!arr1.includes(arr2[j])) {
+      newArr.push(arr2[j]);
+    }
+  }
+  return newArr;
+}
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
