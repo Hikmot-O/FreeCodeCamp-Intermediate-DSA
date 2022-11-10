f33bcbfed7d0b51bf51ba55dd99a9c910203b17a
Hikmot <69507776+Hikmot-O@users.noreply.github.com>
2022-11-10 10:37:10 +0100

    Added solutions to DSA 3, 5 and 6

--- script .js
+++ script .js
@@ -34,3 +34,50 @@ function diffArray(arr1, arr2) {
   return newArr;
 }
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+//Intermediate DSA 3//
+function destroyer(arr, ...arg) {
+  let newArr = [];
+
+  for(let i=0; i<arr.length; i++){
+    if(!arg.includes(arr[i])) newArr.push(arr[i])
+  }
+  return newArr;
+}
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+//Intermediate DSA 5//
+function spinalCase(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' '|| str[i] === '_') str = str.slice(0, i) + str[i + 1].toUpperCase() + str.slice(i + 2, str.length);
+
+  }
+
+  str = str[0].toLowerCase() + str.slice(1).replaceAll(' ', '').replaceAll('_', '');
+  str = str.replace(/[A-Z]/g, s => '-' + s.toLowerCase())
+  return str;
+}
+spinalCase('This Is Spinal Tap');
+
+//Intermediate DSA 6//
+function translatePigLatin(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  if(vowels.includes(str[0])){
+    str = str + 'way';
+  }else{
+    for(let i=0; i<str.length; i++){
+      if(vowels.includes(str[i])){
+        str = str.slice(i, str.length) + str.slice(0, i) + 'ay';
+        break;
+      }
+
+      if(!vowels.includes(str[i]) && i === str.length - 1){
+        str = str + 'ay';
+        break;
+      }
+    }
+  }
+  return str;
+}
+translatePigLatin("consonant");
